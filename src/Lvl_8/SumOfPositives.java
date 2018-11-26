92de495704b4d7e39ae535c99d77d3662bb4406a
Szakalakazo <tomek.starzak@gmail.com>
2018-11-26 18:32:37 +0100

    Added SumOfPositives

--- src/Lvl_8/SumOfPositives.java
+++ src/Lvl_8/SumOfPositives.java
@@ -2,19 +2,28 @@ package Lvl_8;
 
 public class SumOfPositives {
     public static void main(String[] args) {
-        int[] array = {1, -2, 3, 4, 5};
+        int[] array = {1,-2,3,4,5};
         System.out.println(sum(array));
+
     }
 
     public static int sum(int[] array) {
 
         int result = 0;
-        for (int i : array) {
-            if (array[i] > 0) {
-                result = result + array[i];
-                System.out.println(result);
+        for(int i : array) {
+            if(i > 0) {
+                result += i;
             }
         }
         return result;
+
+
+       /* int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > 0) {
+                sum = sum + array[i];
+            }
+        }
+        return sum;*/
     }
 }
