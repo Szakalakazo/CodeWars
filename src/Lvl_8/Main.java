1b0fff1d7914be3c6334572d171f58a9de923754
Szakalakazo <tomek.starzak@gmail.com>
2018-11-27 16:47:09 +0100

    RemoveStringSpaces added

---
+++ src/Lvl_8/Main.java
@@ -0,0 +1,8 @@
+package Lvl_8;
+
+public class Main {
+    public static void main(String[] args) {
+        //Removing Spaces in String
+        //System.out.println(RemoveStringSpaces.noSpace("String With Spaces"));
+    }
+}
---
+++ src/Lvl_8/RemoveStringSpaces.java
@@ -0,0 +1,7 @@
+package Lvl_8;
+
+public class RemoveStringSpaces {
+    static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+}
