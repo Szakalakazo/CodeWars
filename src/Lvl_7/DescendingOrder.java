1bbb60a3a8b67999198a4eb3413b69e38bce270f
Szakalakazo <tomek.starzak@gmail.com>
2018-11-28 18:28:36 +0100

    DescendingOrder started, ti be fixed

--- src/Lvl_7/DescendingOrder.java
+++ src/Lvl_7/DescendingOrder.java
@@ -1,4 +1,22 @@
 package Lvl_7;
 
+import java.util.Arrays;
+
 public class DescendingOrder {
+    public static int sortDesc(final int num) {
+
+        String intString = "";
+        intString.valueOf(num);
+        String[] strArray = intString.split("");
+        int[] intArray = new int[strArray.length];
+
+        for(int i = 0; i < intArray.length-1; i++){
+            intArray[i] = Integer.parseInt(strArray[i]);
+        }
+        Arrays.sort(intArray);
+        return intArray.toString()
+
+
+
+    }
 }
