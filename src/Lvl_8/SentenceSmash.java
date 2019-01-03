5600d2b23143588812607fd37ddbff09a58d3a04
Szakalakazo <tomek.starzak@gmail.com>
2019-01-03 14:23:52 +0100

    SentenceSmasher lvl 8 completed

---
+++ src/Lvl_8/SentenceSmash.java
@@ -0,0 +1,12 @@
+package Lvl_8;
+
+public class SentenceSmash {
+    public static String smash(String[] words) {
+
+        return String.join(" ", words);
+       /* StringBuilder output = new StringBuilder();
+        for(String word : words) output.append(word).append(" ");
+        return output.toString().trim();*/
+
+    }
+}
